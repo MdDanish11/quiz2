39ac87a1c254304ede78b89ebe9c50a380a1d56d
Md Danish <163197683+MdDanishCoder@users.noreply.github.com>
2024-04-02 15:05:57 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,104 @@
+const quizData = [{
+    question: "Which of the following is a client site language?",
+    a: "Java",
+    b: "C",
+    c: "Python",
+    d: "JavaScript",
+    correct: "d",
+},
+{
+    question: "JavaScript is an _____ language?",
+    a: "Object-Oriented",
+    b: "Object-Based",
+    c: "Procedural",
+    d: "None of the above",
+    correct: "a",
+},
+{
+    question: "What year was JavaScript launched?",
+    a: "1996",
+    b: "1995",
+    c: "1994",
+    d: "None of the above",
+    correct: "b",
+},
+{
+    question: "Which of these cannot be used for a variable name in Java?",
+    a: "identifier and keyword",
+    b: "identifier",
+    c: "keyword",
+    d: "none of the above",
+    correct: "c",
+},
+{
+    question: "Which of the following methods can be used to display data in some form using JavaScript?",
+    a: "document.write()",
+    b: "console.log()",
+    c: "window.alert()",
+    d: "All of the above",
+    correct: "d",
+}
+];
+let index = 0;
+let correct = 0,
+incorrect = 0,
+total = quizData.length;
+let questionBox = document.getElementById("questionBox");
+let allInputs = document.querySelectorAll("input[type='radio']")
+const loadQuestion = () => {
+if (total === index) {
+    return quizEnd()
+}
+reset()
+const data = quizData[index]
+questionBox.innerHTML = `${index + 1}) ${data.question}`
+allInputs[0].nextElementSibling.innerText = data.a
+allInputs[1].nextElementSibling.innerText = data.b
+allInputs[2].nextElementSibling.innerText = data.c
+allInputs[3].nextElementSibling.innerText = data.d
+}
+
+document.querySelector("#submit").addEventListener(
+"click",
+function() {
+    const data = quizData[index]
+    const ans = getAnswer()
+    if (ans === data.correct) {
+        correct++;
+    } else {
+        incorrect++;
+    }
+    index++;
+    loadQuestion()
+}
+)
+
+const getAnswer = () => {
+let ans;
+allInputs.forEach(
+    (inputEl) => {
+        if (inputEl.checked) {
+            ans = inputEl.value;
+        }
+    }
+)
+return ans;
+}
+
+const reset = () => {
+allInputs.forEach(
+    (inputEl) => {
+        inputEl.checked = false;
+    }
+)
+}
+
+const quizEnd = () => {
+// console.log(document.getElementsByClassName("container"));
+document.getElementsByClassName("container")[0].innerHTML = `
+    <div class="col">
+        <h3 class="w-100"> Hii, you've scored ${correct} / ${total} </h3>
+    </div>
+`
+}
+loadQuestion(index);
